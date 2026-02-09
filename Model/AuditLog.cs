5d8bf6ee0db11257cb48da9badcf274af4e03213
bel <120295142+mifski@users.noreply.github.com>
2026-02-09 09:58:53 +0800

    Change const strings to readonly for audit actions

--- Model/AuditLog.cs
+++ Model/AuditLog.cs
@@ -62,49 +62,49 @@ namespace AceJob.Model
     public static class AuditActions
     {
         // Authentication Actions
-        public const string Login = "Login";
-        public const string Logout = "Logout";
-        public const string FailedLogin = "FailedLogin";
-        public const string Lockout = "Lockout";
-        public const string LockoutRecovery = "LockoutRecovery";
-        public const string Registration = "Registration";
-        public const string ConcurrentLogin = "ConcurrentLogin";
+        public static readonly string Login = "Login";
+        public static readonly string Logout = "Logout";
+        public static readonly string FailedLogin = "FailedLogin";
+        public static readonly string Lockout = "Lockout";
+        public static readonly string LockoutRecovery = "LockoutRecovery";
+        public static readonly string Registration = "Registration";
+        public static readonly string ConcurrentLogin = "ConcurrentLogin";
         
         // Password Management Actions
         // Use non-sensitive short codes as values to avoid storing the literal word "password" in logs
-        public const string PasswordChange = "PW_CHG";
-        public const string PasswordReset = "PW_RST";
-        public const string PasswordResetRequest = "PW_RST_REQ";
-        public const string ForcedPasswordChange = "PW_CHG_FORCED";
+        public static readonly string PasswordChange = "PW_CHG";
+        public static readonly string PasswordReset = "PW_RST";
+        public static readonly string PasswordResetRequest = "PW_RST_REQ";
+        public static readonly string ForcedPasswordChange = "PW_CHG_FORCED";
         
         // Two-Factor Authentication Actions
-        public const string TwoFactorEnabled = "2FA_Enabled";
-        public const string TwoFactorDisabled = "2FA_Disabled";
-        public const string TwoFactorSetup = "2FA_Setup";
-        public const string TwoFactorLogin = "2FA_Login";
-        public const string TwoFactorRecoveryCodes = "2FA_RecoveryCodes";
-        public const string TwoFactorRecoveryUsed = "2FA_RecoveryUsed";
+        public static readonly string TwoFactorEnabled = "2FA_Enabled";
+        public static readonly string TwoFactorDisabled = "2FA_Disabled";
+        public static readonly string TwoFactorSetup = "2FA_Setup";
+        public static readonly string TwoFactorLogin = "2FA_Login";
+        public static readonly string TwoFactorRecoveryCodes = "2FA_RecoveryCodes";
+        public static readonly string TwoFactorRecoveryUsed = "2FA_RecoveryUsed";
    
         // Profile Actions
-        public const string ProfileView = "ProfileView";
-        public const string ProfileUpdate = "ProfileUpdate";
+        public static readonly string ProfileView = "ProfileView";
+        public static readonly string ProfileUpdate = "ProfileUpdate";
      
         // Security Events
-        public const string SecurityBreach = "SecurityBreach";
-        public const string SuspiciousActivity = "SuspiciousActivity";
-        public const string ReCaptchaFailure = "ReCaptchaFailure";
+        public static readonly string SecurityBreach = "SecurityBreach";
+        public static readonly string SuspiciousActivity = "SuspiciousActivity";
+        public static readonly string ReCaptchaFailure = "ReCaptchaFailure";
       
         // File Operations
-        public const string FileUpload = "FileUpload";
-        public const string FileDownload = "FileDownload";
-        public const string FileDelete = "FileDelete";
+        public static readonly string FileUpload = "FileUpload";
+        public static readonly string FileDownload = "FileDownload";
+        public static readonly string FileDelete = "FileDelete";
         
         // Administrative Actions
-        public const string AdminAction = "AdminAction";
-        public const string UserCreated = "UserCreated";
-        public const string UserUpdated = "UserUpdated";
-        public const string UserDeleted = "UserDeleted";
-        public const string RoleAssigned = "RoleAssigned";
-        public const string RoleRemoved = "RoleRemoved";
+        public static readonly string AdminAction = "AdminAction";
+        public static readonly string UserCreated = "UserCreated";
+        public static readonly string UserUpdated = "UserUpdated";
+        public static readonly string UserDeleted = "UserDeleted";
+        public static readonly string RoleAssigned = "RoleAssigned";
+        public static readonly string RoleRemoved = "RoleRemoved";
     }
 }
